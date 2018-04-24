87cc131663ce45b1aeaaec7b288e52e5f9775603
Henry Sha <35982580+shayh1992@users.noreply.github.com>
2018-04-24 18:48:48 -0400

    Update quote.py

--- quote.py
+++ quote.py
@@ -10,7 +10,7 @@ def display():
     
     with open('inspiration.txt') as fp:
         quotes = fp.read().split('\n')
-    #quotes = quotes.split('\n')
+    
     from random import randrange
     random_index = randrange(0,len(quotes))
     quotes = quotes[random_index]
